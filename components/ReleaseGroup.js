1877109b7f2280156e8f3895773af88e41d5ca38
John Dimm <john.dimm@galigo.com>
2021-10-02 11:36:10 -0700

    Avoid the "by " when there is nothing after.

--- components/ReleaseGroup.js
+++ components/ReleaseGroup.js
@@ -4,11 +4,14 @@ import Artist from 'components/Artist'
 
 const ReleaseGroup = ( {record}) => {
     const link = `/release_group/${record.release_group}`
+    const hlin = record.headliner
+      ? <span> by {record.headliner}</span>
+      : null
 
     return <div className='linked_album'>
           <Link href={link} passHref={true}>
               <div>
-                <b>{record.title}</b> by {record.headliner}
+                <b>{record.title}</b> {hlin}
                 <CoverArt cover_url={record.cover_url} />
               </div>
           </Link>
--- sql/setup/careers.sh
+++ sql/setup/careers.sh
@@ -3,3 +3,4 @@ psql -f careers_rename.sql
 psql -f index2.sql
 psql -f careers_index.sql
 psql -f gin_index.sql
+psql -f functions.sql
